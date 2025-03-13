5fc67bb9b5a64c06b6467f499673f70e440177dd
KrizzK <157081834+krizzk@users.noreply.github.com>
2025-03-13 22:56:10 +0700

    Delete prisma/migrations directory

--- prisma/migrations/20250313011212_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `note` on the `order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `order` DROP COLUMN `note`;
